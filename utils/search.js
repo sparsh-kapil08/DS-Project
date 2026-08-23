51aff6273210906124e3ef8f56ba34a494cc5bf4
SinhaSrijan <150418906+SinhaSrijan@users.noreply.github.com>
2026-08-23 19:48:18 +0530

    Add binarySearch function to search utilities

--- utils/search.js
+++ utils/search.js
@@ -15,3 +15,23 @@ function linearSearch(cars, query, fields = ['brand', 'model', 'type', 'location
 
   return results;
 }
+
+function binarySearch(sortedCars, maxPrice) {
+  let lo  = 0;
+  let hi  = sortedCars.length - 1;
+  let ans = -1;
+
+  while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    if (sortedCars[mid].pricePerDay <= maxPrice) {
+      ans = mid;
+      lo  = mid + 1;
+    } else {
+      hi  = mid - 1;
+    }
+  }
+
+  return ans === -1 ? [] : sortedCars.slice(0, ans + 1);
+}
+
+module.exports = { linearSearch, binarySearch };
